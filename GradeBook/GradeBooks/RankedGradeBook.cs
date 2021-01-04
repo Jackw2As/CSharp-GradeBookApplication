bb55c6fb9ad14466023b87f58905c821b6f28324
Jackw2As <aaldersjack@gmail.com>
2021-01-04 12:43:08 +0000

    Attempt 1 At Calculating Letter Grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,58 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name) 
         {
             Type = GradeBookType.Ranked;
+        }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            return CalculateLetterGrade(averageGrade);
+        }
+
+        private char CalculateLetterGrade(double averageGrade)
+        {
+            SortedDictionary<int, double> Grades = new SortedDictionary<int, double>();
+            foreach (var student in Students)
+            {
+                student.Grades.ForEach(s => Grades.Add(int.Parse(s.ToString()), s));
+            }
+
+            int TotalNumberofGrades = Grades.Values.Count;
+
+            int splitter = TotalNumberofGrades / 5; //This returns value at 20%
+
+            if (averageGrade >= Grades.Values.ElementAt(splitter))
+            {
+                return 'A';
+            }
+
+            splitter += TotalNumberofGrades / 5; //This returns value at 40%
+
+            if (averageGrade >= Grades.Values.ElementAt(splitter))
+            {
+                return 'B';
+            }
+
+            splitter += TotalNumberofGrades / 5; //This returns value at 60%
+
+            if (averageGrade >= Grades.Values.ElementAt(splitter))
+            {
+                return 'C';
+            }
+
+            splitter += TotalNumberofGrades / 5; //This returns value at 80%
+
+            if (averageGrade >= Grades.Values.ElementAt(splitter))
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
