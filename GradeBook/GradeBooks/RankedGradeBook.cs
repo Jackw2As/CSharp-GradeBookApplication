b5f3ed200713c153e14a3e4dd5f485dc0cb0605b
Jackw2As <aaldersjack@gmail.com>
2021-01-04 14:50:51 +0000

    Fixed Code

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,8 +1,6 @@
 ﻿using GradeBook.Enums;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace GradeBook.GradeBooks
 {
@@ -13,36 +11,28 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
-            {
-                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            if (Students.Count < 5)  
+            { 
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work."); 
             }
 
-            return CalculateLetterGrade(averageGrade);
-        }
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-        private char CalculateLetterGrade(double averageGrade)
-        {
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToArray();
+            int threshold = (int)Math.Ceiling(Students.Count * 0.2);
 
-            int gradesCount = grades.Count();
-
-            int threshold = (int)Math.Ceiling(gradesCount * 0.2); //This returns value at 20%
-
-
-            if (averageGrade >= grades[threshold - 1]) return 'A';
-
-            threshold += gradesCount / 5; //This returns value at 40%
-            if (averageGrade >= grades[(threshold * 2) - 1]) return 'B';
-
-            threshold += gradesCount / 5; //This returns value at 60%
-            if (averageGrade >= grades[(threshold * 3) - 1])  return 'C';
-
-            threshold += gradesCount / 5; //This returns value at 80%
-            if (averageGrade >= grades[(threshold * 4) - 1]) return 'D';
+            if (averageGrade >= grades[threshold - 1]) 
+                return 'A';
+            
+            if (averageGrade >= grades[(threshold * 2) - 1]) 
+                return 'B';
+           
+            if (averageGrade >= grades[(threshold * 3) - 1])  
+                return 'C';
+            
+            if (averageGrade >= grades[(threshold * 4) - 1]) 
+                return 'D';
 
             return 'F';
         }
